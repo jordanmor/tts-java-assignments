63a1466bc56b75d8e108777e7287b13b485069b2
Jordan Mor <jordanmor@gmail.com>
2019-05-22 13:47:22 -0400

    Add Project 1

---
+++ PROJECT_ONE/src/com/tts/AsciiChars.java
@@ -0,0 +1,14 @@
+package com.tts;
+
+// Create a separate class that will print valid characters to the user
+public class AsciiChars {
+	public static void printNumbers() {
+	    // TODO: print valid numeric input
+	}
+	public static void printLowerCase() {
+	    // TODO: print valid lowercase alphabetic input
+	}
+	public static void printUpperCase() {
+	    // TODO: print valid uppercase alphabetic input
+	}
+}
