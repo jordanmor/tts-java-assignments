f1804c2cbe0b356b0416fdb15672a0e10fcaf607
Jordan Mor <jordanmor@gmail.com>
2019-05-23 09:02:36 -0400

    Project 1 - Complete ASCII chart challenge

--- PROJECT_ONE/src/com/tts/AsciiChars.java
+++ PROJECT_ONE/src/com/tts/AsciiChars.java
@@ -4,11 +4,25 @@ package com.tts;
 public class AsciiChars {
 	public static void printNumbers() {
 	    // TODO: print valid numeric input
+		char[] numberChars = {'0','1','2','3','4','5','6','7','8','9'};
+		printChars(numberChars);
 	}
-	public static void printLowerCase() {
-	    // TODO: print valid lowercase alphabetic input
-	}
+	
 	public static void printUpperCase() {
 	    // TODO: print valid uppercase alphabetic input
+		char[] lowerCaseChars = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+		printChars(lowerCaseChars);
+	}
+	
+	public static void printLowerCase() {
+	    // TODO: print valid lowercase alphabetic input
+		char[] lowerCaseChars = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		printChars(lowerCaseChars);
+	}
+	
+	private static void printChars(char arr[]) {
+		for(int x = 0; x < arr.length; x++) {
+			System.out.println("The character " + arr[x] + " has an ascii value of " + (int) arr[x]);
+		}
 	}
 }
---
+++ PROJECT_ONE/src/com/tts/Main.java
@@ -0,0 +1,12 @@
+package com.tts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AsciiChars.printNumbers();
+		AsciiChars.printUpperCase();
+		AsciiChars.printLowerCase();
+	}
+
+}
