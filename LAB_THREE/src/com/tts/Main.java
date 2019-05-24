3498074c4c2dc39099defeaafd99154018606580
Jordan Mor <jordanmor@gmail.com>
2019-05-24 11:37:51 -0400

    Complete LAB 3 Steps 2 - 3

--- LAB_THREE/src/com/tts/Main.java
+++ LAB_THREE/src/com/tts/Main.java
@@ -71,6 +71,91 @@ public class Main {
 		/* =====-=====-=====-=====-=====
 		   ********** STEP 2 ***********
 		   =====-=====-=====-=====-===== */
+		
+		// Write a for loop that prints out the numbers 1-30.
+		System.out.println("Prints out the numbers 1-30:");
+		for(int x = 1; x <= 30; x++) {
+			System.out.println(x);
+		}
+		// Write a for loop that prints out the even numbers 1-30
+		System.out.println("Prints out the even numbers between 1-30:");
+		for(int x = 1; x <= 30; x++) {
+			if(x % 2 == 0) {
+				System.out.println(x);				
+			}
+		}
+		
+		// Write a loop that counts down from 20 to 0
+		System.out.println("Count down from 20 to 0:");
+		for(int x = 20; x >= 0; x--) {
+			System.out.println(x);
+		}
+		
+		// Write a for loop with an && operator that contains two statements
+		for (int x = 1, y = 1; x < 20 && y < 50; x++, y += 4)
+		{
+		  System.out.println("X is: " + x + ", y is: " + y);
+		}
+		
+		/* =====-=====-=====-=====-=====
+		   ********** STEP 3 ***********
+		   =====-=====-=====-=====-===== */
+		
+		// Declare a very long String of at least 10 words
+		 String str = "This is a very long and completely meaningless string that must serve a higher purpose.";
+		 
+		// Write a statement using an enhanced for loop that will print out each character on its own line. 
+		// Note that the enhanced for loop will not work on a String so you will have to consult the documentation 
+		// on how to get an iterable collection of characters from a string.
+		 
+		char[] charArray = str.toCharArray();
+		for(char character: charArray ) {
+			System.out.println(character);
+		}
+		
+		// Use the looping structure from above and alter the process just a bit. For each vowel, 
+		// print the capital form of it instead of the lower case. Try writing the logic with both 
+		// an if statement and then a switch statement. Which is easier?
+		
+		// The for loop is definitely more efficient solution.
+		System.out.println("For Loop Uppercase Vowels");
+		for(char character: charArray ) {
+			if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+				int UpperChar = (int) character - 32;
+				System.out.println((char) UpperChar);
+			} else {
+				System.out.println(character);				
+			}
+		}
+		
+		System.out.println("Switch Uppercase Vowels");
+		for(char character: charArray ) {
+			switch(character) {
+				case 'a':
+					int UpperChar = (int) character - 32;
+					System.out.println((char) UpperChar);
+					break;
+				case 'e':
+					UpperChar = (int) character - 32;
+					System.out.println((char) UpperChar);
+					break;
+				case 'i':
+					UpperChar = (int) character - 32;
+					System.out.println((char) UpperChar);
+					break;
+				case 'o':
+					UpperChar = (int) character - 32;
+					System.out.println((char) UpperChar);
+					break;
+				case 'u':
+					UpperChar = (int) character - 32;
+					System.out.println((char) UpperChar);
+					break;
+				default:
+					System.out.println(character);
+			}
+		}
+		
 	}
 
 }
